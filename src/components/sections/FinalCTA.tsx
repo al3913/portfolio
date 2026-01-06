85443ed375ced739cd08c9f675623179a9530b5a
al3913 <al3913@g.rit.edu>
2026-01-05 23:19:19 -0500

    adjusted some spacing on FinalCTA and refactored original typewriter animation to use max-width instead of width

--- src/components/sections/FinalCTA.tsx
+++ src/components/sections/FinalCTA.tsx
@@ -5,7 +5,7 @@ import Typewriter from "../Typewriter";
 
 export default function FinalCTA() {
     return (
-        <div className="flex flex-col gap-[60px]">
+        <div className="flex flex-col gap-[80px]">
             <div className="flex flex-col text-hero text-brand-light-blue items-start w-full">
                 <p> If you have a problem worth solving — </p>
                 <p>
@@ -13,15 +13,14 @@ export default function FinalCTA() {
                     <span className="font-light"> small, </span>
                     or in between
                 </p>
-                <p className="mt-[20px]">
+                <p className="mt-[40px]">
                     I'd love to hear your story.
                 </p>
             </div>
             <div className="flex flex-col items-center w-full gap-[40px] relative">
                 <a href="https://linkedin.com/in/andylin21" target="_blank" rel="noopener noreferrer">
-                <button className="bg-brand-light-blue px-[70px] py-[20px] rounded-[59.6px] text-hero leading-[48px]"> Let's connect </button>
+                    <button className="bg-brand-light-blue px-[70px] py-[20px] rounded-[59.6px] text-hero leading-[48px]"> Let's connect </button>
                 </a>
-                <div className="absolute bottom-0 right-0"><Cursor variant="You"/></div>
                 <div className="flex gap-[16px]">
                     <Typewriter className="text-h2 bg-brand-black" words={["what are you waiting for?"]}/>
                     <Cursor variant="You"/>
