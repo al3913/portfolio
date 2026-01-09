2bfa4fbc73fe0ffcaefb89a333ffb02668ed8f44
al3913 <al3913@g.rit.edu>
2026-01-08 23:53:46 -0500

    pulse test

--- src/components/sections/FinalCTA.tsx
+++ src/components/sections/FinalCTA.tsx
@@ -19,7 +19,7 @@ export default function FinalCTA() {
             </div>
             <div className="flex flex-col items-center w-full gap-[40px] relative">
                 <a href="https://linkedin.com/in/andylin21" target="_blank" rel="noopener noreferrer">
-                    <button className="bg-brand-light-blue px-[70px] py-[20px] rounded-[59.6px] text-hero leading-[48px]"> Let's connect </button>
+                    <button className="bg-brand-light-blue px-[70px] py-[20px] rounded-[59.6px] text-hero leading-[48px] animate-pulse"> Let's connect </button>
                 </a>
                 <div className="flex gap-[16px] items-start">
                     {/* <Typewriter className="text-h2 !bg-brand-black !text-brand-light-blue overflow-hidden" words={["what are you waiting for?"]}/> */}
