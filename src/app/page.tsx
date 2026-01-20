1a0530857afd7de9275b961cf40516a08a8cdff3
al3913 <al3913@g.rit.edu>
2026-01-19 23:58:34 -0500

    moved skills into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Maintenance from '@/components/sections/Maintenance';
 import Hero from '@/components/sections/Hero';
 import AboutMe from '@/components/sections/AboutMe';
 import FinalCTA from '@/components/sections/FinalCTA';
+import Skills from '@/components/sections/Skills';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="flex bg-brand-black min-h-screen flex-col py-[310px] gap-[315px] items-center m-auto justify-start px-[60px] md:px-[120px] relative max-w-[1440px]">
           <Hero />
           <AboutMe />
+          <Skills />
           <FinalCTA />
           {/* <Maintenance /> */}
       </main>
