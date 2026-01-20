40f70b50cfb3d8602268fc0d998f75dd9682b245
al3913 <al3913@g.rit.edu>
2026-01-20 00:37:55 -0500

    added icon library also fixed corrupted chevron svg

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,7 +1,17 @@
+import { CodeXml, PanelsTopLeft } from "lucide-react";
+import Image from "next/image";
+
 export default function Skills() {
     return (
-        <div>
-            <h1>Skills</h1>
-        </div>
+        <>
+            <div className="flex gap-[48px] items-center w-full ml-[-40px]">
+                <Image src="/icons/chevron.svg" alt="Chevron" width={64} height={64}/>
+                <CodeXml color="white" size={64}/>
+            </div>
+            <div className="flex gap-[48px] items-center w-full ml-[-40px]">
+                <Image src="/icons/chevron.svg" alt="Chevron" width={64} height={64}/>
+                <PanelsTopLeft color="white" size={64} strokeWidth={1.5}/>
+            </div>
+        </>
     )
 }
