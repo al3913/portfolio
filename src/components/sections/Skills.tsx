779ce91c57f5adfc79b91cde8c2edfa026e1d457
al3913 <al3913@g.rit.edu>
2026-01-25 17:57:04 -0500

    added icons and setup for skill sections

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -9,13 +9,18 @@ export default function Skills() {
         
     return (
         <>
-            <div className="flex gap-[48px] items-center w-full ml-[-40px]">
-                <Image src="/icons/chevron.svg" alt="Chevron" width={64} height={64}/>
-                <h2 className="text-hero text-brand-light-blue">{'<'}<span className="text-[#96e1ff]">Engineer</span>{'/>'}</h2>
+            <div className="flex flex-col gap-[32px] w-full">
+                <div className="flex gap-[32px] items-center w-full ml-[-12px] pb-[32px]">
+                    <Image src="/icons/chevron.svg" alt="Chevron" width={64} height={64} className="rotate-90"/>
+                    <h2 className="text-hero text-[#ad86ff] border-3 border-brand-light-blue p-[16px]">design</h2>
+                </div>
+                
             </div>
-            <div className="flex gap-[48px] items-center w-full ml-[-40px]">
-                <Image src="/icons/chevron.svg" alt="Chevron" width={64} height={64}/>
-                <h2 className="text-hero text-[#ad86ff] border-3 border-brand-light-blue p-[16px]">design</h2>
+            <div className="flex flex-col gap-[32px] w-full">
+                <div className="flex gap-[32px] items-center w-full ml-[-12px]">
+                    <Image src="/icons/chevron.svg" alt="Chevron" width={64} height={64} className="rotate-90"/>
+                    <h2 className="text-hero text-brand-light-blue">{'<'}<span className="text-[#96e1ff]">Engineer</span>{'/>'}</h2>
+                </div>
             </div>
         </>
     )
