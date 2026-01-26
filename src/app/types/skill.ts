9c69f4a77d34907f8bceaeb07238cd98df28cbec
al3913 <al3913@g.rit.edu>
2026-01-26 01:01:17 -0500

    created skill type and created file for skilllist

---
+++ src/app/types/skill.ts
@@ -0,0 +1,4 @@
+export type SkillItem = {
+    icon: string
+    name: string
+}
---
+++ src/components/SkillList.tsx
@@ -0,0 +1,11 @@
+import { SkillItem } from "@/app/types/skill";
+
+type Props = {
+    items: SkillItem[]
+}
+
+const SkillList = ({items} : Props) => {
+    return 
+}
+
+export default SkillList;
