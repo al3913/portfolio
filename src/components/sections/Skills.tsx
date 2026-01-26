a7bd06591f010a0a5881206887e799e2e1e4d364
al3913 <al3913@g.rit.edu>
2026-01-26 00:02:30 -0500

    added sub categories for design skills

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,4 +1,4 @@
-import { CodeXml, PanelsTopLeft } from "lucide-react";
+import { PenTool, PanelsTopLeft } from "lucide-react";
 import Image from "next/image";
 
 export default function Skills() {
@@ -10,11 +10,22 @@ export default function Skills() {
     return (
         <>
             <div className="flex flex-col gap-[32px] w-full">
-                <div className="flex gap-[32px] items-center w-full ml-[-12px] pb-[32px]">
+                <div className="flex gap-[32px] items-center w-full ml-[-12px]">
                     <Image src="/icons/chevron.svg" alt="Chevron" width={64} height={64} className="rotate-90"/>
                     <h2 className="text-hero text-[#ad86ff] border-3 border-brand-light-blue p-[16px]">design</h2>
                 </div>
-                
+                <div className="pl-[80px]">
+                    <div className="flex gap-[36px]">
+                        <PanelsTopLeft color="#ffffff" size={58}/>
+                        <h3 className="text-hero text-brand-light-blue">Principles</h3>
+                    </div>
+                </div>
+                <div className="pl-[80px]">
+                    <div className="flex gap-[36px]">
+                        <PenTool color="#ffffff" size={58}/>
+                        <h3 className="text-hero text-brand-light-blue">Tools</h3>
+                    </div>  
+                </div>    
             </div>
             <div className="flex flex-col gap-[32px] w-full">
                 <div className="flex gap-[32px] items-center w-full ml-[-12px]">
