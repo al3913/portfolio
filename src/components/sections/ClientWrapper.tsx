53a252daa42d948b36291abccf7bebe3fcc633fd
al3913 <al3913@g.rit.edu>
2026-01-31 13:22:11 -0500

    added screen for when people are in mobile view, need to fix weird flash of wrong screen though

--- src/components/sections/ClientWrapper.tsx
+++ src/components/sections/ClientWrapper.tsx
@@ -1,9 +1,11 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import MobileWarning from "./MobileWarning";
 
 export default function ClientWrapper({ children }: { children: React.ReactNode }) {
+    const [isMobile, setIsMobile] = useState(true);
 
-    useEffect(() => {
+    useEffect(() => { 
         const handleEnter = () => document.body.style.cursor = "url('/cursors/UserCursor.png') 0 0, auto";
         const handleLeave = () => document.body.style.cursor = "auto";
         document.addEventListener("mouseenter", handleEnter);
@@ -14,6 +16,30 @@ export default function ClientWrapper({ children }: { children: React.ReactNode 
           document.removeEventListener("mouseleave", handleLeave);
         };
       }, []);
+
+    useEffect(() => {
+        const checkViewport = () => {
+            setIsMobile(window.innerWidth < 1440);
+        };
+
+        // Check on mount
+        checkViewport();
+
+        // Listen for resize events
+        window.addEventListener("resize", checkViewport);
+
+        return () => {
+            window.removeEventListener("resize", checkViewport);
+        };
+    }, []);
+
+    if (isMobile) {
+        return (
+            <main className="flex bg-brand-black min-h-screen flex-col items-center justify-center px-[60px] md:px-122 relative">
+                <MobileWarning/>
+            </main>
+        );
+    }
 
     return <>{children}</>;
 }
---
+++ src/components/sections/MobileWarning.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+
+// Image asset from Figma
+const maintenanceImage = "/chibis/chibi-maintenance.png";
+
+interface MaintenanceProps {
+  className?: string;
+}
+
+export default function MobileWarning({ className = '' }: MaintenanceProps) {
+  return (
+    <div 
+      className={`flex flex-col gap-2_5 items-center justify-center relative w-full h-full ${className}`}
+      data-name="Maintenance" 
+      data-node-id="66:88"
+    >
+      <Image 
+        src={maintenanceImage} 
+        alt="Maintenance" 
+        width={350} 
+        height={350}
+        data-name="Untitled_Artwork 4 1" 
+        data-node-id="228:113"
+      />
+      <div 
+        className="flex flex-col gap-10 text-brand-light-blue text-center text-hero"
+        data-node-id="228:115"
+      >
+        <p className="text-2xl lg:text-4xl md:leading-[40px] lg:leading-[60px]">
+          This page is taking a quick break <br className="hidden md:block"/> while I fix things up.
+        </p>
+        <p className="text-h2 text-xl lg:text-2xl">
+          Checkout the view on PC 🙂
+        </p>
+      </div>
+    </div>
+  );
+}
