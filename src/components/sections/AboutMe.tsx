430b95cb377ea05658a6f09ca89dedd4290b6e3f
al3913 <al3913@g.rit.edu>
2026-02-07 02:17:34 -0500

    added headline to about me

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -6,10 +6,18 @@ export default function AboutMe() {
     return (
         <div className="flex flex-col w-full">
             <div className="flex w-full justify-between items-center">
-                <p className="text-body text-brand-light-blue max-w-[471px] text-[18px]">
-                    {"I’m a software engineer who blends full-stack development with a strong focus on design and usability. Whether it’s building tools that save hours of work or creating apps that feel intuitive from the first tap, I aim for solutions that leave a lasting impact on the people who use them."}
-                </p>
-                <Image src="/other/About Me Photo.svg" alt="Chibi Right" width={530} height={412}/>
+                <div className="flex flex-col h-[412px] items-start justify-start gap-[32px]">
+                    <div className="flex gap-[12px] items-center h-[68px]">
+                        <p className="text-headline text-brand-light-blue">
+                            {"Who's this guy?"}
+                        </p>
+                        <Image src="/chibis/chibi-down.png" alt="Chibi Andy pointing down" width={68} height={68} className="h-[58px] object-cover object-top"/>
+                    </div>
+                    <p className="text-body text-brand-light-blue max-w-[471px] text-[18px]">
+                        {"I’m a software engineer who blends full-stack development with a strong focus on design and usability. Whether it’s building tools that save hours of work or creating apps that feel intuitive from the first tap, I aim for solutions that leave a lasting impact on the people who use them."}
+                    </p>
+                </div>
+                <Image src="/other/About Me Photo.svg" alt="Picture of Andy" width={530} height={412}/>
             </div>
             <div className="grid grid-cols-[11px_1fr] gap-[48px]">
                 <div className="flex flex-col w-full h-full relative justify-between items-center py-[60.305px]">
