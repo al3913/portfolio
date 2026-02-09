01570eb5c8eff0ccd6bc6a7be9692e7dd7903cb7
al3913 <al3913@g.rit.edu>
2026-02-08 20:54:46 -0500

    refactor component structure so there isnt scrollability when there shouldnt be

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,13 @@ import Skills from '@/components/sections/Skills';
 export default function Home() {
   return (
     <div className="bg-brand-black">
-      <main className="flex bg-brand-black min-h-screen flex-col py-[310px] gap-[315px] items-center m-auto justify-start px-[60px] md:px-[120px] relative max-w-[1440px]">
-          <Hero />
-          <AboutMe />
-          <Skills />
-          <FinalCTA />
+      <main className="flex bg-brand-black min-h-screen flex-col py-[310px] items-center m-auto justify-start px-[60px] md:px-[120px] relative max-w-[1440px]">
+          <div className="hidden w-full h-full gap-[315px] laptop:flex flex-col">
+            <Hero />
+            <AboutMe />
+            <Skills />
+            <FinalCTA />
+          </div>
           <div className="z-100 fixed bg-brand-black inset-0 laptop:hidden">
             <MobileWarning/>
           </div>
